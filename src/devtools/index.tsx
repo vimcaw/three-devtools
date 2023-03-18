04ba2d075d9fa947eb981e08cdada32a1d099953
vimcaw <vimcaw@gmail.com>
2023-03-18 14:53:02 +0800

    feat(style): improve the style of popup and devtools panel

--- src/devtools/index.tsx
+++ src/devtools/index.tsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  Content,
+  defaultTheme,
+  Flex,
+  Heading,
+  IllustratedMessage,
+  Provider,
+  View,
+} from '@adobe/react-spectrum';
+import NoSearchResults from '@spectrum-icons/illustrations/NoSearchResults';
+import 'modern-normalize';
 import DevtoolsPort from './DevtoolsPort';
 
 function App() {
@@ -15,12 +26,22 @@ function App() {
     };
   }, [devtoolsPort]);
 
-  return connectedThreeJsReversion ? (
-    <p>
-      Connected ThreeJs <code>r{connectedThreeJsReversion}</code>
-    </p>
-  ) : (
-    <p>Please open a page that use three.js</p>
+  return (
+    <Provider theme={defaultTheme} height="100vh">
+      {connectedThreeJsReversion ? (
+        <View padding="size-200" height="100%">
+          Connected ThreeJs <code>r{connectedThreeJsReversion}</code>
+        </View>
+      ) : (
+        <Flex height="100vh" justifyContent="center" alignItems="center">
+          <IllustratedMessage>
+            <NoSearchResults />
+            <Heading>Three.js not detected</Heading>
+            <Content>Please open a page that use three.js</Content>
+          </IllustratedMessage>
+        </Flex>
+      )}
+    </Provider>
   );
 }
 
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { defaultTheme, Provider, Text, View } from '@adobe/react-spectrum';
+import 'modern-normalize';
+import './index.css';
 
 function App() {
   return (
-    <p>
-      Please open `Chrome DevTools` and navigate to the `three` tab in order to access the Three.js
-      Devtools.
-    </p>
+    <Provider theme={defaultTheme}>
+      <View width="size-3000" paddingX="size-300" paddingY="size-200">
+        Please open <Text UNSAFE_className="code">Chrome DevTools</Text> and navigate to the{' '}
+        <Text UNSAFE_className="code">three</Text> tab in order to access the Three.js Devtools.
+      </View>
+    </Provider>
   );
 }
 
