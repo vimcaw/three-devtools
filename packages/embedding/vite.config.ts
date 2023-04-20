ed0fb150572b1891a18e069523566828ad3b579d
vimcaw <vimcaw@gmail.com>
2023-04-21 00:23:32 +0800

    chore: use `dts-bundle-generator` to generate better `dts` file

--- packages/embedding/vite.config.ts
+++ packages/embedding/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +15,6 @@ export default defineConfig({
         ],
       ],
     }),
-    dts({ compilerOptions: { moduleResolution: 99 } }),
   ],
   build: {
     lib: {
