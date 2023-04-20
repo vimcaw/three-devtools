dd2146aca4e28aa9255056de307fede8c5bce464
vimcaw <vimcaw@gmail.com>
2023-04-20 17:35:23 +0800

    fix: remove normalize css to avoid CSS pollution

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,5 +1,4 @@
 import { ConfigProvider, Layout, theme } from 'antd';
-import 'modern-normalize';
 import './index.css';
 import type { Object3D, Scene, WebGLRenderer } from 'three';
 import { useMedia } from 'react-use';
