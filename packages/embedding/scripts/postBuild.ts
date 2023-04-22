426295bb48feb054b8c54e3c7823aee4074d8f1a
vimcaw <vimcaw@gmail.com>
2023-04-22 23:21:30 +0800

    chore: copy all necessary files for publishing

--- packages/embedding/scripts/postBuild.ts
+++ packages/embedding/scripts/postBuild.ts
@@ -1,11 +1,23 @@
-import { readFile, writeFile } from 'node:fs/promises';
+import { readFile, writeFile, cp } from 'node:fs/promises';
+import { join } from 'node:path';
 import { PackageJson } from 'type-fest';
 import sortPackageJson from 'sort-package-json';
 import { pick } from 'lodash-es';
 
 (async () => {
+  const rootPath = '../../';
+  const copiedFileNames = [
+    'README.md',
+    'LICENSE',
+    'CHANGELOG.md',
+    'CONTRIBUTING.md',
+    'CODE_OF_CONDUCT.md',
+    'resource',
+  ];
   const devPackageJson = JSON.parse(await readFile('package.json', 'utf8')) as PackageJson;
-  const rootPackageJson = JSON.parse(await readFile('../../package.json', 'utf8')) as PackageJson;
+  const rootPackageJson = JSON.parse(
+    await readFile(join(rootPath, 'package.json'), 'utf8')
+  ) as PackageJson;
   const prodPackageJsonTemplate = JSON.parse(
     await readFile('package.prod.json', 'utf8')
   ) as PackageJson;
@@ -29,4 +41,10 @@ import { pick } from 'lodash-es';
   await writeFile('dist/package.json', JSON.stringify(prodPackageJson, null, 2), {
     encoding: 'utf8',
   });
+
+  await Promise.all(
+    copiedFileNames.map(async fileName => {
+      await cp(join(rootPath, fileName), `dist/${fileName}`, { recursive: true }).catch();
+    })
+  );
 })();
