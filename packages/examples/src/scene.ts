ee893a1486170f429db86196173a799cda1ec50e
李承峰 <44059402+cf-19@users.noreply.github.com>
2023-05-18 15:43:04 +0800

    chore: add example code

--- packages/examples/src/scene.ts
+++ packages/examples/src/scene.ts
@@ -7,49 +7,88 @@ import {
   MeshStandardMaterial,
   HemisphereLight,
 } from 'three';
+import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 export class DefaultScene {
-  constructor() {
+  constructor() {}
+
+  init() {
+    this.mixer = null;
+
+    this.clock = new THREE.Clock();
+    this.container = document.getElementById('container');
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.camera.position.z = 5;
-    const geometry = new BoxGeometry(1, 1, 1);
-    const material = new MeshStandardMaterial({ color: 0xffffff });
-    const light = new HemisphereLight(0xffffff, 0x000000, 1);
-    light.position.set(5, 5, 5);
-    this.cube = new Mesh(geometry, material)
 
-    for (let i = 0; i < 5; i ++) {
-      const m = new Mesh(geometry, material);
-      m.position.set(i, i, i);
-      this.scene.add(m)
-    }
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
 
-    this.scene.add(this.cube);
-    this.scene.add(light);
-    this.onResize = this.onResize.bind(this);
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xbfe3dd);
+    this.scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
+
+    this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100);
+    this.camera.position.set(5, 2, 8);
+
+    const controls = (this.controls = new OrbitControls(this.camera, this.renderer.domElement));
+    controls.target.set(0, 0.5, 0);
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('https://threejs.org/examples/jsm/libs/draco/gltf/');
+
+    const loader = new GLTFLoader();
+    loader.setDRACOLoader(dracoLoader);
+    loader.load(
+      'https://threejs.org/examples/models/gltf/LittlestTokyo.glb',
+      gltf => {
+        const model = gltf.scene;
+        model.position.set(1, 1, 0);
+        model.scale.set(0.01, 0.01, 0.01);
+        this.scene.add(model);
+
+        this.mixer = new THREE.AnimationMixer(model);
+        this.mixer.clipAction(gltf.animations[0]).play();
+        this.animate();
+      },
+      undefined,
+      e => {
+        console.error(e);
+      }
+    );
   }
 
-  private readonly scene = new Scene();
-
-  private readonly camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-
-  private renderer = new WebGLRenderer();
-
-  private readonly cube: Mesh;
-
-  private container?: HTMLElement;
+  // private readonly scene = new Scene();
+  //
+  // private readonly camera = new PerspectiveCamera(
+  //   75,
+  //   window.innerWidth / window.innerHeight,
+  //   0.1,
+  //   1000
+  // );
+  //
+  // private renderer = new WebGLRenderer();
+  //
+  // private readonly cube: Mesh;
+  //
+  // private container?: HTMLElement;
 
   animate() {
     requestAnimationFrame(this.animate.bind(this));
 
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    const delta = this.clock.getDelta();
 
+    this.mixer.update(delta);
+    this.controls.update();
+    this.stats.update();
     this.renderer.render(this.scene, this.camera);
   }
 
@@ -61,9 +100,11 @@ export class DefaultScene {
 
   mount(container: HTMLElement) {
     this.container = container;
-    container.appendChild(this.renderer.domElement);
-    this.animate();
+    this.stats = new Stats();
+    container.appendChild(this.stats.dom);
     window.addEventListener('resize', this.onResize, false);
+    this.init();
+    container.appendChild(this.renderer.domElement);
   }
 
   unmount() {
